622433839fd575c1173176a021c93431aa0dd975
niclas-cpu <niclasjosephbrites@gmail.com>
2025-12-15 11:01:48 -0100

    Update App.py

--- App.py
+++ App.py
@@ -6,5 +6,5 @@ menu = option_menu(menu_title="Menu",
                    icons = ["house", "bar-chart", "bar-chart-line", "toggles", "bar-chart"],
                    menu_icon = "cast",
                    default_index=0,
-                   orientation="vertical"
+                   orientation="diagonal"
                   )
